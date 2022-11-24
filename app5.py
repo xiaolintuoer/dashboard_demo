fc8c7304dde1d59ca57132752bc224f6e39705ed
xiaolintuoer <coconutslw@gmail.com>
2022-11-24 17:37:17 +0800

    初始化项目

---
+++ app5.py
@@ -0,0 +1,85 @@
+from dash import Dash, html, dcc, Input, Output
+import pandas as pd
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+
+app.layout = html.Div([
+    html.Div([
+        
+        html.Div([
+            dcc.Dropdown(
+                df['Indicator Name'].unique(),
+                'Fertility rate, total (births per women)',
+                id='crossfilter-xaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-xaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                df['Indicator Name'].unique(),
+                'Life expectancy at birth, total (years)',
+                id='crossfilter-yaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-yaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={'padding': '10px 5px'}),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(dcc.Slider(
+        df['Year'].min(),
+        df['Year'].max(),
+        step=None,
+        id='crossfilter-year-slider',
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+])
+
+
+
+@app.callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'),
+    Input('crossfilter-yaxis-type', 'value'),
+    Input('crossfilter-year-slider', 'value')
+)
+def update_graph(xaxis_column_name, yaxis_column_name, xaxis_type, yaxis_type, year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
+    )
+
+
