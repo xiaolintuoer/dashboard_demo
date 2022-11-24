b2704ffd88b7b85b150bff9928672e42db776abc
xiaolintuoer <coconutslw@gmail.com>
2022-11-24 18:26:58 +0800

    '1'

---
+++ my_demo/demo.py
@@ -0,0 +1,10 @@
+# 11月份数据分析看板的demo
+
+from dash import Dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
