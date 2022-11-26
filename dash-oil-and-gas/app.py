7b1d38d73052b432a08d48fff6658e1c1a6bb58b
xiaolintuoer <coconutslw@gmail.com>
2022-11-26 16:11:08 +0800

    '2'

--- dash-oil-and-gas/app.py
+++ dash-oil-and-gas/app.py
@@ -8,8 +8,8 @@ import math
 import datetime as dt
 import pandas as pd
 from dash.dependencies import Input, Output, State, ClientsideFunction
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
+
 
 # Multi-dropdown options
 from controls import COUNTIES, WELL_STATUSES, WELL_TYPES, WELL_COLORS
